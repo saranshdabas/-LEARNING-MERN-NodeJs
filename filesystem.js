b04a06b7c779bacf6eb2fe8a0e620a14f5b3c6bd
Apple <sdjee2015@gmail.com>
2020-12-14 23:29:26 +0530

    File system module to read files

---
+++ filesystem.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+//readFile takes 2 args, location of file and a callback fn
+fs.readFile(__dirname + "/hello.txt", "utf8", (err, data) => {
+  if (err) throw err;
+  console.log(data);
+});
+
+//Might as well display in the browser
+const http = require("http");
+
+http
+  .createServer((req, res) => {
+    fs.readFile(__dirname + "/hello.txt", "utf8", (err, data) => {
+      if (err) throw err;
+      res.writeHead(200, { "content-type": "text/plain" });
+      res.write(data);
+      res.end();
+    });
+  })
+  .listen(3000);
